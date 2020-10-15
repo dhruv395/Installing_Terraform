86c3a0e7dbfb872c25f7342aa97ca8e127279f61
Dhruba Jyoti Ray <36419707+dhruv395@users.noreply.github.com>
2020-10-15 13:48:45 +0530

    vpc with 3 public and 3 private subnet

--- vpc/myvpc.tf
+++ vpc/myvpc.tf
@@ -1,24 +1,24 @@
 #internet vpc
 resource "aws_vpc" "raytech_prod-vpc" {
-	cidr_block = "10.0.0.0/16"
-	instance_tenancy = "default"
-	enable_dns_support = "true"
-	enable_dns_hostnames = "true"
-	enable_classiclink = "false"
-	tags =  {
-  		  Name = "raytech_prod-vpc"
-	}
+        cidr_block = "10.0.0.0/16"
+        instance_tenancy = "default"
+        enable_dns_support = "true"
+        enable_dns_hostnames = "true"
+        enable_classiclink = "false"
+        tags =  {
+                  Name = "raytech_prod-vpc"
+        }
 }
 
 #subnets
 resource "aws_subnet" "ap-south-1a-pub-sub" {
-	vpc_id = "${aws_vpc.raytech_prod-vpc.id}"
-	cidr_block = "10.0.1.0/24"
-	map_public_ip_on_launch = "true"
-	availability_zone = "ap-south-1a"
-	tags = {
-		Name = "ap-south-1a-pub-sub"
- 	}
+        vpc_id = "${aws_vpc.raytech_prod-vpc.id}"
+        cidr_block = "10.0.1.0/24"
+        map_public_ip_on_launch = "true"
+        availability_zone = "ap-south-1a"
+        tags = {
+                Name = "pub-sub_ap-south-1a"
+        }
 }
 
 resource "aws_subnet" "ap-south-1b-pub-sub" {
@@ -27,8 +27,8 @@ resource "aws_subnet" "ap-south-1b-pub-sub" {
         map_public_ip_on_launch = "true"
         availability_zone = "ap-south-1b"
         tags = {
-                Name = "ap-south-1b-pub-sub"
-	}
+                Name = "pub-sub_ap-south-1b"
+        }
 }
 
 resource "aws_subnet" "ap-south-1c-pub-sub" {
@@ -37,8 +37,8 @@ resource "aws_subnet" "ap-south-1c-pub-sub" {
         map_public_ip_on_launch = "true"
         availability_zone = "ap-south-1c"
         tags = {
-                Name = "ap-south-1c-pub-sub"
-	}
+                Name = "pub-sub_ap-south-1c"
+        }
 }
 
 ##private
@@ -49,8 +49,8 @@ resource "aws_subnet" "ap-south-1a-priv-sub" {
         map_public_ip_on_launch = "true"
         availability_zone = "ap-south-1a"
         tags = {
-                Name = "ap-south-1a-priv-sub"
-	}
+                Name = "priv-sub_ap-south-1a"
+        }
 }
 
 resource "aws_subnet" "ap-south-1b-priv-sub" {
@@ -59,8 +59,8 @@ resource "aws_subnet" "ap-south-1b-priv-sub" {
         map_public_ip_on_launch = "true"
         availability_zone = "ap-south-1b"
         tags = {
-                Name = "ap-south-1b-priv-sub"
-	}
+                Name = "priv-sub_ap-south-1b"
+        }
 }
 
 resource "aws_subnet" "ap-south-1c-priv-sub" {
@@ -69,36 +69,36 @@ resource "aws_subnet" "ap-south-1c-priv-sub" {
         map_public_ip_on_launch = "true"
         availability_zone = "ap-south-1c"
         tags = {
-                Name = "ap-south-1c-priv-sub"
-	}
+                Name = "priv-sub_ap-south-1c"
+        }
 }
 
 
 ## internet GW
 resource "aws_internet_gateway" "raytech_IGW" {
-	vpc_id = "${aws_vpc.raytech_prod-vpc.id}"
-	tags = {
-		Name = "raytech_IGW"
-	}
+        vpc_id = "${aws_vpc.raytech_prod-vpc.id}"
+        tags = {
+                Name = "raytech_IGW"
+        }
 }
 
 
 ##route tables
 resource "aws_route_table" "raytech_pubRT" {
-	vpc_id = "${aws_vpc.raytech_prod-vpc.id}"
-	route {
-		cidr_block = "0.0.0.0/0"
-		gateway_id = "${aws_internet_gateway.raytech_IGW.id}"
+        vpc_id = "${aws_vpc.raytech_prod-vpc.id}"
+        route {
+                cidr_block = "0.0.0.0/0"
+                gateway_id = "${aws_internet_gateway.raytech_IGW.id}"
   }
-	tags = {
-		Name = "raytech_pubRT"
-	}
+        tags = {
+                Name = "raytech_pubRT"
+        }
 }
 
 ##route association of public subnet
 resource "aws_route_table_association" "ap-south-1a-pub-subRT" {
-	subnet_id = "${aws_subnet.ap-south-1a-pub-sub.id}"
-	route_table_id = "${aws_route_table.raytech_pubRT.id}"
+        subnet_id = "${aws_subnet.ap-south-1a-pub-sub.id}"
+        route_table_id = "${aws_route_table.raytech_pubRT.id}"
 }
 
 resource "aws_route_table_association" "ap-south-1b-pub-subRT" {
@@ -110,4 +110,3 @@ resource "aws_route_table_association" "ap-south-1c-pub-subRT" {
         subnet_id = "${aws_subnet.ap-south-1c-pub-sub.id}"
         route_table_id = "${aws_route_table.raytech_pubRT.id}"
 }
-
